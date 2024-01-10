fe752fa41e40f17fd02424f5a2fd0db3fdfd48a1
Seif El Islam <145872339+SeifELIslam-c@users.noreply.github.com>
2024-01-10 23:32:40 +0100

    Add files via upload

---
+++ words counter (mo3awe9)/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Please enter your words: ");
+string user = Convert.ToString(Console.ReadLine()); Console.Write("\n");
+
+int wordsNumber = wordcounter(user);
+Console.WriteLine("you entered " + wordsNumber + " words");
+
+
+static int wordcounter(string words)
+{
+    int resualt = 1;
+    for(int i = 0; i < words.Length; i++)
+    {
+        if (words[i] == ' ')
+        {
+            resualt++;
+        }
+        else
+        {
+            continue;
+        }
+        
+    }
+    return resualt;
+}
